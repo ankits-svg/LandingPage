397fce17c1df017e44907cea19907cdf6568f2c6
Ankit Sharma <ankits745@gmail.com>
2023-11-04 22:04:54 +0530

    Beginning of creating landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
+
 
 function App() {
+  const helloRef = useRef(null);
+  const app = useRef(null);
+  // console.log(helloRef.current)
+  let t1 = useRef(null);
+  
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      
+      t1.current = gsap
+        .timeline()
+        .to(".hello1", {
+          duration: 2,
+          rotation: 360,
+        })
+        .to(".hello2", {
+          x: 300,
+        });
+    }, app);
+
+    return () => ctx.revert();
+  }, []);
+
+  const handleClick = (e) => {
+    gsap.to(e.target, {
+      rotation: "150",
+      yoyo: true,
+      repeat: 1,
+    });
+  };
+
+  const onEnter=(e)=>{
+    gsap.to(e.target,{
+      scale:1.5
+    })
+  }
+
+  const onLeave=(e)=>{
+    gsap.to(e.target,{
+      scale:1
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={app}>
+      <h1 className="hello1">Hello world</h1>
+      <h1 className="hello2">Hello world 2</h1>
+      <button
+        onMouseEnter={onEnter}
+        onMouseLeave={onLeave}
+        onClick={handleClick}
+      >
+        Hi!! what are you doing
+      </button>
     </div>
   );
 }
